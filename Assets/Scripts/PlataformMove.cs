bf2681c8f3d3a2a5072a5a877aa903a24afa56d3
ismaDLR <ismadelosreyes@gmail.com>
2025-01-24 01:05:37 +0100

    Map and plataforms
    Error en el movimiento de las plataformas FALTA ARREGLAR

---
+++ Assets/Scripts/PlataformMove.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlataformMove : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 2.5f;
+    [SerializeField]
+    private float maxPosition = 1f;
+    [SerializeField]
+    private float minPosition = -1f;
+
+    private Rigidbody2D rigidbody;
+    private float dir = 1; // Horizontal move direction value [-1, 1]
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+
+        if (gameObject.transform.position.x > maxPosition)
+        {
+            dir *= -1;
+            
+        }
+        else if(gameObject.transform.position.x < minPosition)
+        {
+            dir *= -1;
+        }
+        
+        Vector2 velocity = rigidbody.linearVelocity;
+        velocity.x = dir * speed;
+        rigidbody.linearVelocity = velocity;
+    }
+}
